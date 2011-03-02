1c4010386c7a0ab43f8ddcaffe7ef1bec6c4d1ae
Israel Tsadok <itsadok@gmail.com>
2011-03-02 18:57:24 +0200

    First commit

---
+++ secretshandler.py
@@ -0,0 +1,23 @@
+from google.appengine.ext import db
+
+class Secret(db.Model):
+    secret = db.StringProperty()
+    name = db.StringProperty()
+
+def getSecret(name):
+    a = db.GqlQuery("SELECT * FROM Secret WHERE name='%s'"%name).get()
+    return a.secret
+
+
+def setSecret(name,secret):
+    a = Secret()
+    a.name = name
+    a.secret = secret
+    a.put()
+
+
+def changeSecret(name, secret):
+    a = getSecret(name)
+    if a:
+        a.secret = secret
+        a.put()
